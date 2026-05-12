68da39430b8eb64958cf393f43cd9290074a85d3
Minh Nguyen Cong <mcong@box.com>
2026-05-12 15:14:23 +0200

    Remove ui-elements

--- src/ui-elements/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Box UI Elements - React components for Box content.
- *
- * TODO: Re-export from @box/ui-elements when ready
- * @packageDocumentation
- */
-
-// Placeholder - will re-export from @box/ui-elements
-export {};
